35395724d4b64b42b76146b31037f1fa2f4fe6a9
DEEPAK ACHARYA <56695253+deepakacharyab@users.noreply.github.com>
2024-10-16 15:17:55 -0500

    all

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Deep Learning", "Frontend Development", "Backend Development", "Machine Learning"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
